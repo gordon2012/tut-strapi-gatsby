103ffeee130f4dcc0b2fbe3d3293fe058e736353
Gordon Doskas <gordon2012@users.noreply.github.com>
2020-05-10 11:16:47 -0700

    Nav component
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Seo from './seo';
+import Nav from './nav';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Seo />
+      <Nav />
       <main>{children}</main>
     </>
   );
